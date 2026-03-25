db9a1c2094ce92c321a4ce9e19e209af087b59e1
PedroBiotec <joaopedromg.office1@gmail.com>
2026-03-25 17:08:56 -0300

    Create 01 - Introduction to R language.R

---
+++ 01 - Introduction to R language.R
@@ -0,0 +1,7 @@
+Function on R is a command, that can or not receive a argument, a input, but will give us a output, which is a result of a modification on the object or the program, given that instruction for the modification caused by the function has already been programmed.
+
+- Fuction Média Aritmética:
+```R
+mean(1:8)
+```
+
